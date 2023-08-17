b583dbc503d735e89237905adcfc0bc7b18f9f5a
mjalalzai <33738574+MForensic@users.noreply.github.com>
2023-08-16 20:28:23 -0700

    Update UniformRandomSelection.go

--- RobustSolitarDistribution/UniformRandomSelection.go
+++ RobustSolitarDistribution/UniformRandomSelection.go
@@ -5,40 +5,7 @@ import (
 	"sort"
 )
 
-/*
-func ChooseDegree(DegreeDistribution []float32) int{
-	UintDegreeDistribution:=floatTouintDegreeDistribution(DegreeDistribution)
-	//fmt.Println("UintDegreeDistribution",  UintDegreeDistribution)
-	SlicesOfChoices:=createSliceOfChoices(UintDegreeDistribution)
-	chooser, _:=wr.NewChooser(SlicesOfChoices...)
-	result := chooser.Pick().(int)
-	return result
 
-}
-
-func floatTouintDegreeDistribution(DegreeDistribution []float32)  []uint{
-	UintDegreeDistribution:=make([]uint,0,len(DegreeDistribution))
-	for _,v :=range DegreeDistribution{
-		UintDegreeDistribution=append(UintDegreeDistribution,uint(v))
-	}
-	return UintDegreeDistribution
-
-}
-
-func createSliceOfChoices(floatTouintDegreeDistribution []uint) []wr.Choice{
-	SliceChoices:=make([]wr.Choice,len(floatTouintDegreeDistribution),len(floatTouintDegreeDistribution))
-	fmt.Println("floatTouintDegreeDistribution is", floatTouintDegreeDistribution)
-	for idx, value :=range floatTouintDegreeDistribution{
-		SliceChoices[idx].Item=idx
-		SliceChoices[idx].Weight=value
-	}
-	return SliceChoices
-}
-*/
-
-// pickDegree returns the smallest index i such that cdf[i] > r
-// (r a random number from the random generator)
-// cdf must be sorted in ascending order.
 
 func PickDegree(random *rand.Rand, cdf []float64) int {
 	r := random.Float64()
