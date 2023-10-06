82d4bcbf85cc81983f822759dfeb597e7ded1801
Saeed Yazdinezhad <saeedyazdinezhad@gmail.com>
2023-10-05 18:36:55 -0700

    Debugging

--- Net/Client.go
+++ Net/Client.go
@@ -22,34 +22,64 @@ import (
 
 //}
 
-func MsgSender(conn net.Conn, Msg E.VerifyEntity, peer, nodeID, port string, IdToConnMap *map[string]net.Conn, MapIdToEncoder *map[string]*gob.Encoder) {
-	////	ctx,cancel:=context.WithCancel(context.Background())
-	//defer cancel()
-	//	fmt.Println("Conn is, ", conn)
-	//		fmt.Println("msg is,", Msg)
+// func MsgSender(conn net.Conn, Msg E.VerifyEntity, peer, nodeID, port string, IdToConnMap *map[string]net.Conn, MapIdToEncoder *map[string]*gob.Encoder) {
+// 	////	ctx,cancel:=context.WithCancel(context.Background())
+// 	//defer cancel()
+// 	//	fmt.Println("Conn is, ", conn)
+// 	//		fmt.Println("msg is,", Msg)
 
-	//	Encoder := gob.NewEncoder(conn)
+// 	//	Encoder := gob.NewEncoder(conn)
+// 	enc := (*MapIdToEncoder)[nodeID]
+// 	// fmt.Println("Encoder is", enc)
+// 	// fmt.Println("Encoder type is", reflect.TypeOf(enc))
+// 	// fmt.Println("Msg type is", reflect.TypeOf(Msg))
+// 	err := enc.Encode(&Msg)
+// 	// err := encoder.Encode(&Msg)
+// 	if err != nil {
+// 		fmt.Println("Encoding error is", err.Error())
+// 		conn.Close()
+// 		conn = nil
+// 		time.Sleep(300 * time.Millisecond)
+// 		conn = DialNode(peer, port)
+// 		fmt.Println("Creating new Connection")
+// 		enc := gob.NewEncoder(conn)
+// 		(*IdToConnMap)[nodeID] = conn
+// 		(*MapIdToEncoder)[nodeID] = enc
+// 		MsgSender(conn, Msg, peer, nodeID, port, IdToConnMap, MapIdToEncoder)
+// 		//return enc, true
+// 	}
+// 	//	conn.Close()
+// 	//return enc, false
+// }
+
+func MsgSender(conn net.Conn, Msg E.VerifyEntity, peer, nodeID, port string, IdToConnMap *map[string]net.Conn, MapIdToEncoder *map[string]*gob.Encoder) {
 	enc := (*MapIdToEncoder)[nodeID]
-	// fmt.Println("Encoder is", enc)
-	// fmt.Println("Encoder type is", reflect.TypeOf(enc))
-	// fmt.Println("Msg type is", reflect.TypeOf(Msg))
-	err := enc.Encode(&Msg)
-	// err := encoder.Encode(&Msg)
+
+	// First send the dataType
+	dataType := "VerifyEntity" // This is the data type identifier for E.VerifyEntity
+	err := enc.Encode(&dataType)
 	if err != nil {
-		fmt.Println("Encoding error is", err.Error())
-		conn.Close()
-		conn = nil
-		time.Sleep(300 * time.Millisecond)
-		conn = DialNode(peer, port)
-		fmt.Println("Creating new Connection")
-		enc := gob.NewEncoder(conn)
-		(*IdToConnMap)[nodeID] = conn
-		(*MapIdToEncoder)[nodeID] = enc
-		MsgSender(conn, Msg, peer, nodeID, port, IdToConnMap, MapIdToEncoder)
-		//return enc, true
+		handleEncodingError(err, conn, peer, port, Msg, nodeID, IdToConnMap, MapIdToEncoder)
+		return
 	}
-	//	conn.Close()
-	//return enc, false
+
+	// Then send the actual message
+	err = enc.Encode(&Msg)
+	if err != nil {
+		handleEncodingError(err, conn, peer, port, Msg, nodeID, IdToConnMap, MapIdToEncoder)
+	}
+}
+
+func handleEncodingError(err error, conn net.Conn, peer, port string, Msg E.VerifyEntity, nodeID string, IdToConnMap *map[string]net.Conn, MapIdToEncoder *map[string]*gob.Encoder) {
+	fmt.Println("Encoding error:", err)
+	conn.Close()
+	time.Sleep(300 * time.Millisecond)
+	newConn := DialNode(peer, port)
+	fmt.Println("Creating new Connection")
+	newEnc := gob.NewEncoder(newConn)
+	(*IdToConnMap)[nodeID] = newConn
+	(*MapIdToEncoder)[nodeID] = newEnc
+	MsgSender(newConn, Msg, peer, nodeID, port, IdToConnMap, MapIdToEncoder)
 }
 
 func KZGZSender(conn net.Conn, Z kzg.KZGZSender, peer, nodeID, port string, IdToConnMap *map[string]net.Conn, MapIdToEncoder *map[string]*gob.Encoder) {
--- Net/Server.go
+++ Net/Server.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"reflect"
 
 	E "github.com/xm0onh/LT-Code/Encoding"
 	kzg "github.com/xm0onh/LT-Code/KZG"
@@ -69,47 +68,102 @@ func  TimertListenerLoop(L net.Listener,conInterface CNI.NetworkToConActInterfac
 
 */
 
+// func Blockhandleconnection(conn net.Conn, conInterface CNI.NetworkToConActInterface, CommitteeSize int) {
+
+// 	//	fmt.Println("inside Blockhandleconnection decoder")
+// 	var RecType1 E.VerifyEntity
+// 	var RecType2 kzg.KZGZSender
+// 	decoder := gob.NewDecoder(conn)
+// 	//////To be done: Read deadline
+// 	for {
+
+// 		err := decoder.Decode(&RecType1)
+// 		err2 := decoder.Decode(&RecType2)
+// 		// fmt.Println("Received Block is", RecType1)
+// 		fmt.Println("type is", reflect.TypeOf(RecType2))
+// 		if err != nil || err2 != nil {
+// 			if err.Error() == "gob: unknown type id or corrupted data" {
+// 				fmt.Println("Error during BftMsgshandleconnection", err)
+
+// 			} else {
+// 				fmt.Println("here")
+// 				conn.Close()
+// 				conn = nil
+// 				return
+// 			}
+// 		}
+// 		fmt.Println("type is", reflect.TypeOf(RecType1))
+// 		fmt.Println("type is", reflect.TypeOf(RecType2))
+
+// 		fmt.Println("Conn in Server.go ", conn)
+// 		ipaddress, ErrBin := GetIPaddFromConn(conn)
+// 		if !ErrBin {
+// 			log.Fatal("Conn corruption in Server")
+// 		}
+// 		//		fmt.Println("Received Msg", RecType1)
+// 		fmt.Println("Committee Size is", CommitteeSize)
+// 		fmt.Println("IP address is ", ipaddress)
+// 		go conInterface.PassMsgToActor(RecType2, CommitteeSize, ipaddress)
+
+// 		go conInterface.PassMsgToActor(RecType1, CommitteeSize, ipaddress)
+
+// 	}
+
+// }
+
 func Blockhandleconnection(conn net.Conn, conInterface CNI.NetworkToConActInterface, CommitteeSize int) {
-
-	//	fmt.Println("inside Blockhandleconnection decoder")
-	var RecType1 E.VerifyEntity
-	var RecType2 kzg.KZGZSender
 	decoder := gob.NewDecoder(conn)
-	//////To be done: Read deadline
 	for {
+		// Decode a type identifier string first
+		var dataType string
+		err := decoder.Decode(&dataType)
+		if err != nil {
+			fmt.Println("Error decoding data type:", err)
+			conn.Close()
+			return
+		}
 
-		err := decoder.Decode(&RecType1)
-		err2 := decoder.Decode(&RecType2)
-		// fmt.Println("Received Block is", RecType1)
-		fmt.Println("type is", reflect.TypeOf(RecType2))
-		if err != nil || err2 != nil {
-			if err.Error() == "gob: unknown type id or corrupted data" {
-				fmt.Println("Error during BftMsgshandleconnection", err)
-
-			} else {
-				fmt.Println("here")
-				conn.Close()
-				conn = nil
-				return
+		switch dataType {
+		case "VerifyEntity":
+			var RecType1 E.VerifyEntity
+			err = decoder.Decode(&RecType1)
+			if err != nil {
+				handleDecodeError(err, conn)
+				continue // Go to the next loop iteration
 			}
+			handleReceivedData(RecType1, conn, conInterface, CommitteeSize)
+
+		case "KZGZSender":
+			var RecType2 kzg.KZGZSender
+			err = decoder.Decode(&RecType2)
+			if err != nil {
+				handleDecodeError(err, conn)
+				continue // Go to the next loop iteration
+			}
+			handleReceivedData(RecType2, conn, conInterface, CommitteeSize)
+
+		default:
+			fmt.Println("Unknown data type received:", dataType)
 		}
-		fmt.Println("type is", reflect.TypeOf(RecType1))
-		fmt.Println("type is", reflect.TypeOf(RecType2))
+	}
+}
 
-		fmt.Println("Conn in Server.go ", conn)
-		ipaddress, ErrBin := GetIPaddFromConn(conn)
-		if !ErrBin {
-			log.Fatal("Conn corruption in Server")
-		}
-		//		fmt.Println("Received Msg", RecType1)
-		fmt.Println("Committee Size is", CommitteeSize)
-		fmt.Println("IP address is ", ipaddress)
-		go conInterface.PassMsgToActor(RecType2, CommitteeSize, ipaddress)
+func handleDecodeError(err error, conn net.Conn) {
+	if err.Error() == "gob: unknown type id or corrupted data" {
+		fmt.Println("Error during BftMsgshandleconnection:", err)
+	} else {
+		fmt.Println("Decode error occurred.")
+		conn.Close()
+	}
+}
 
-		go conInterface.PassMsgToActor(RecType1, CommitteeSize, ipaddress)
-
+func handleReceivedData(data interface{}, conn net.Conn, conInterface CNI.NetworkToConActInterface, CommitteeSize int) {
+	ipaddress, ErrBin := GetIPaddFromConn(conn)
+	if !ErrBin {
+		log.Fatal("Conn corruption in Server")
 	}
 
+	go conInterface.PassMsgToActor(data, CommitteeSize, ipaddress)
 }
 
 /*
